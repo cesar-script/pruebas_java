7e94417119cab385d8caf2274d3c7b4488529934
Oscar <Oscar@PC-Oscar>
2018-03-25 19:53:38 -0300

    Agregando algunos cambios.

--- src/inicio/Principal.java
+++ src/inicio/Principal.java
@@ -13,5 +13,6 @@ public class Principal {
     public static void main(String[] args) {
         
         System.out.println("Hola Mundo.");
+        System.out.println("Trabajando con GitHub.");
     }
 }
